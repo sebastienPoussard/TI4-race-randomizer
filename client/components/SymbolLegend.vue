2536d6f7c1d6e6f7cfc4c3c640eecdbc1414f9a6
SebPou <36790379+sebastienPoussard@users.noreply.github.com>
2024-12-19 23:08:50 +0100

    Update SymbolLegend.vue
    adding Discordant Stars Factions (UNOFFICIAL)

--- client/components/SymbolLegend.vue
+++ client/components/SymbolLegend.vue
@@ -3,5 +3,6 @@
 		<p class='[ ma0 lh-copy f7 ]'><b>1</b> Recommended for new players</p>
 		<p class='[ ma0 lh-copy f7 ]'><b>+</b> Prophecy of Kings</p>
 		<p class='[ ma0 lh-copy f7 ]'><b>=</b> Codex</p>
+		<p class='[ ma0 lh-copy f7 ]'><b>⭐</b> Discordant Stars Factions (UNOFFICIAL) </p>
 	</div>
 </template>
