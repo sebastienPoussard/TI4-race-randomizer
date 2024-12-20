a0b9ac3f0f12033130e26d7075d7163384775c00
sebastienPoussard <sebastien.poussard@protonmail.ch>
2024-12-20 10:30:07 +0100

    update dist

--- client/components/SymbolLegend.vue
+++
@@ -1,8 +0,0 @@
-<template>
-	<div class='[ mt3 mb3 tl ]'>
-		<p class='[ ma0 lh-copy f7 ]'><b>1</b> Recommended for new players</p>
-		<p class='[ ma0 lh-copy f7 ]'><b>+</b> Prophecy of Kings</p>
-		<p class='[ ma0 lh-copy f7 ]'><b>=</b> Codex</p>
-		<p class='[ ma0 lh-copy f7 ]'><b>⭐</b> Discordant Stars Factions (UNOFFICIAL) </p>
-	</div>
-</template>
